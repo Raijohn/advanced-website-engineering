c800db4bab11002ef68c4d3bbe0a4687de1ffa4b
Raijohn <166853579+Raijohn@users.noreply.github.com>
2026-06-26 18:56:07 +0200

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,122 @@
+var CorrectAns = 0
+var InorrectAns = 0
+
+
+
+var Q1Ans = 0
+
+function Correct1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Correct";
+       Q1Ans = Q1Ans + 1       
+       CorrectAns = CorrectAns + 1 
+    };
+
+}
+function Incorrect1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Incorrect";
+       Q1Ans = Q1Ans + 1      
+       InorrectAns = InorrectAns + 1
+    };
+}
+
+
+
+var Q2Ans = 0
+
+function Correct2(){
+    if(Q2Ans == 0){
+       document.getElementById("A2").innerHTML="Correct";
+       Q2Ans = Q2Ans + 1       
+       CorrectAns = CorrectAns + 1 
+    };
+
+}
+function Incorrect2(){
+    if(Q2Ans == 0){
+       document.getElementById("A1").innerHTML="Incorrect";
+       Q1Ans = Q1Ans + 1      
+       InorrectAns = InorrectAns + 1
+    };
+}
+
+
+
+var Q1Ans = 0
+
+function Correct1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Correct";
+       Q1Ans = Q1Ans + 1       
+       CorrectAns = CorrectAns + 1 
+    };
+
+}
+function Incorrect1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Incorrect";
+       Q1Ans = Q1Ans + 1      
+       InorrectAns = InorrectAns + 1
+    };
+}
+
+
+
+var Q1Ans = 0
+
+function Correct1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Correct";
+       Q1Ans = Q1Ans + 1       
+       CorrectAns = CorrectAns + 1 
+    };
+
+}
+function Incorrect1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Incorrect";
+       Q1Ans = Q1Ans + 1      
+       InorrectAns = InorrectAns + 1
+    };
+}
+
+
+
+var Q1Ans = 0
+
+function Correct1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Correct";
+       Q1Ans = Q1Ans + 1       
+       CorrectAns = CorrectAns + 1 
+    };
+
+}
+function Incorrect1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Incorrect";
+       Q1Ans = Q1Ans + 1      
+       InorrectAns = InorrectAns + 1
+    };
+}
+
+
+
+var Q1Ans = 0
+
+function Correct1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Correct";
+       Q1Ans = Q1Ans + 1       
+       CorrectAns = CorrectAns + 1 
+    };
+
+}
+function Incorrect1(){
+    if(Q1Ans == 0){
+       document.getElementById("A1").innerHTML="Incorrect";
+       Q1Ans = Q1Ans + 1      
+       InorrectAns = InorrectAns + 1
+    };
+}
